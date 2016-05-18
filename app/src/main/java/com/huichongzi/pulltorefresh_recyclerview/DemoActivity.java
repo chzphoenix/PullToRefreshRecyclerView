0bec92c62f5e660bb9d9b9d4fe0193edc4ee4936
chzphoenix <chzphoenix@163.com>
2016-05-18 11:39:00 +0800

    new project

---
+++ app/src/main/java/com/huichongzi/pulltorefresh_recyclerview/DemoActivity.java
@@ -0,0 +1,112 @@
+package com.huichongzi.pulltorefresh_recyclerview;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.huichongzhi.pulltorefresh_recyclerview.library.PullToRefreshRecyclerView;
+import com.huichongzhi.pulltorefresh_recyclerview.library.WrapRecyclerView;
+
+public class DemoActivity extends AppCompatActivity {
+
+    private PullToRefreshRecyclerView pullToRefreshRecyclerView;
+
+    private WrapRecyclerView recyclerView;
+
+    private View header1;
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            pullToRefreshRecyclerView.onRefreshComplete();
+            recyclerView.addHeaderView(header1);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+        initView();
+    }
+
+
+    private void initView(){
+        pullToRefreshRecyclerView = (PullToRefreshRecyclerView)findViewById(R.id.pulltorefresh_recyclerview);
+        pullToRefreshRecyclerView.setMode(PullToRefreshBase.Mode.BOTH);
+        pullToRefreshRecyclerView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<WrapRecyclerView>() {
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<WrapRecyclerView> refreshView) {
+                mHandler.sendEmptyMessageDelayed(0, 500);
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<WrapRecyclerView> refreshView) {
+                mHandler.sendEmptyMessageDelayed(0, 500);
+            }
+        });
+        recyclerView = pullToRefreshRecyclerView.getRefreshableView();
+        MyAdapter adapter = new MyAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                super.onDraw(c, parent, state);
+                Paint p = new Paint();
+                p.setColor(0xffcccccc);
+                int size = parent.getChildCount();
+                for(int i = 0; i < size; i++){
+                    int top = parent.getChildAt(i).getBottom();
+                    int right = parent.getChildAt(i).getRight();
+                    c.drawRect(0, top, right, top + 4, p);
+                }
+            }
+        });
+        recyclerView.setAdapter(adapter);
+
+        header1 = LayoutInflater.from(this).inflate(R.layout.wrap_recycleview_header, null);
+        recyclerView.addHeaderView(header1);
+    }
+
+
+    class MyAdapter extends RecyclerView.Adapter<MyHolder>{
+
+        @Override
+        public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(DemoActivity.this).inflate(R.layout.wrap_recycleview_item, null);
+            MyHolder holder = new MyHolder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyHolder holder, int position) {
+            holder.tv.setText("item:" + position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
+
+        TextView tv;
+        public MyHolder(View itemView) {
+            super(itemView);
+            tv = (TextView)itemView.findViewById(R.id.item_text);
+        }
+    }
+}
---
+++ library/src/main/java/com/huichongzhi/pulltorefresh_recyclerview/library/PullToRefreshRecyclerView.java
@@ -0,0 +1,87 @@
+package com.huichongzhi.pulltorefresh_recyclerview.library;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+
+/**
+ * @author chz
+ * @description
+ * @date 2016/3/15 14:10
+ */
+public class PullToRefreshRecyclerView extends PullToRefreshBase<WrapRecyclerView>{
+    public PullToRefreshRecyclerView(Context context) {
+        super(context);
+    }
+
+    public PullToRefreshRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PullToRefreshRecyclerView(Context context, Mode mode) {
+        super(context, mode);
+    }
+
+    public PullToRefreshRecyclerView(Context context, Mode mode, AnimationStyle animStyle) {
+        super(context, mode, animStyle);
+    }
+
+    @Override
+    public Orientation getPullToRefreshScrollDirection() {
+        return Orientation.VERTICAL;
+    }
+
+    @Override
+    protected WrapRecyclerView createRefreshableView(Context context, AttributeSet attrs) {
+        WrapRecyclerView recyclerView = new WrapRecyclerView(context, attrs);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if(isReadyForPullStart()){
+                    recyclerView.clearFocus();
+                }
+            }
+        });
+        recyclerView.setId(R.id.pulltorefresh_recyclerview);
+        return recyclerView;
+    }
+
+
+
+    @Override
+    protected boolean isReadyForPullEnd() {
+        int lastPosition = getRefreshableView().getLastVisiblePosition();
+        RecyclerView.LayoutManager layoutManager = getRefreshableView().getLayoutManager();
+        View lastView = layoutManager.findViewByPosition(lastPosition);
+        if(lastView != null) {
+            int lastBottom = lastView.getBottom();
+            return lastPosition == getRefreshableView().getRealItemCount() - 1 && lastBottom <= getRefreshableView().getBottom();
+        }
+        else{
+            return true;
+        }
+    }
+
+    @Override
+    protected boolean isReadyForPullStart() {
+        int firstPosition = getRefreshableView().getFirstVisiblePosition();
+        RecyclerView.LayoutManager layoutManager = getRefreshableView().getLayoutManager();
+        View firstView = layoutManager.findViewByPosition(firstPosition);
+        if(firstView != null) {
+            int firstTop = firstView.getTop();
+            return firstPosition == 0 && firstTop >= 0;
+        }
+        else{
+            return true;
+        }
+    }
+
+}
